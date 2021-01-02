2cf0e80d9c67cc1e41e57c337d47cd96dd698078
eunpyo <eunpyo@umich.edu>
2021-01-02 23:51:23 +0900

    Simple router, decoupling router and server js through functional programming

---
+++ router.js
@@ -0,0 +1,4 @@
+function route(pathname){//route by the pathname of url
+    console.log("About to route a request for " + pathname);
+}
+exports.route = route;
---
+++ server.js
@@ -0,0 +1,26 @@
+var http = require("http"); //requires http module shipped by Node.js and make it accessible by varialbe `http`
+var url = require("url");
+
+function start(route){
+    function onRequest(request, response){
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(pathname);
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);//Opens up the server on port 8888 and keep running the server there
+    console.log("Server has started.");
+}
+
+exports.start = start;
+
+/*
+http.createServer returns an object that has an method name `listen`, and takes a port number to listen to
+*/
+
+
+
